cad5ff7905ceac74f19bd43a3295fcfba29428d1
M. Shekhar Suman Reddy <110338285+shekhar3112@users.noreply.github.com>
2025-03-18 21:11:19 +0530

    dashboard page update

---
+++ components/create-account-drawer.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+
+
+const CreateAccountDrawer = ({children}) => {
+
+const [open, setOpen] = useState(false);
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+            <DrawerTitle>Create New Account</DrawerTitle>
+        </DrawerHeader> 
+        <div>
+            <form action=""></form>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CreateAccountDrawer;
