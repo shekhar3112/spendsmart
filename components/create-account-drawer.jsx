59d3b1ec7d6a360debb15e3d9d8931fc92e2c28c
M. Shekhar Suman Reddy <110338285+shekhar3112@users.noreply.github.com>
2025-03-18 22:45:32 +0530

    updated acc

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -8,11 +8,23 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer";
-
+import { useForm } from "react-hook-form";
+import { zodResolver} from "@hookform/resolvers/zod"
+import { accountSchema } from "@/app/lib/schema";
 
 const CreateAccountDrawer = ({children}) => {
 
 const [open, setOpen] = useState(false);
+
+useForm({
+    resolver: zodResolver(accountSchema),
+    defaultValues:{
+        name: "", 
+        type: "CURRENT",
+        balance: "",
+        isDefault: false,
+    },
+})
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>{children}</DrawerTrigger>
